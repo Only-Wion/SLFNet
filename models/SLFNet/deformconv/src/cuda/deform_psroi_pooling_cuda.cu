f79f0c72b53a8efac9d2eb2eface89c6dc1ed63f
Wion Only <wiononly@gmail.com>
2026-07-27 15:19:17 +0800

    Update DCN for modern PyTorch CUDA APIs

--- models/SLFNet/deformconv/src/cuda/deform_psroi_pooling_cuda.cu
+++ models/SLFNet/deformconv/src/cuda/deform_psroi_pooling_cuda.cu
@@ -14,10 +14,7 @@
 
 #include <ATen/ATen.h>
 #include <ATen/cuda/CUDAContext.h>
-
-#include <THC/THC.h>
-#include <THC/THCAtomics.cuh>
-#include <THC/THCDeviceUtils.cuh>
+#include <c10/cuda/CUDAException.h>
 
 #define CUDA_KERNEL_LOOP(i, n)                        \
   for (int i = blockIdx.x * blockDim.x + threadIdx.x; \
@@ -307,11 +304,11 @@ deform_psroi_pooling_cuda_forward(const at::Tensor &input,
 
   if (out.numel() == 0)
   {
-    THCudaCheck(cudaGetLastError());
+    C10_CUDA_KERNEL_LAUNCH_CHECK();
     return std::make_tuple(out, top_count);
   }
 
-  dim3 grid(std::min(THCCeilDiv(out_size, 512L), 4096L));
+  dim3 grid(std::min((out_size + 511L) / 512L, 4096L));
   dim3 block(512);
 
   AT_DISPATCH_FLOATING_TYPES(input.type(), "deform_psroi_pooling_cuda_forward", [&] {
@@ -336,7 +333,7 @@ deform_psroi_pooling_cuda_forward(const at::Tensor &input,
         out.data<scalar_t>(),
         top_count.data<scalar_t>());
   });
-  THCudaCheck(cudaGetLastError());
+  C10_CUDA_KERNEL_LAUNCH_CHECK();
   return std::make_tuple(out, top_count);
 }
 
@@ -380,11 +377,11 @@ deform_psroi_pooling_cuda_backward(const at::Tensor &out_grad,
 
   if (input_grad.numel() == 0)
   {
-    THCudaCheck(cudaGetLastError());
+    C10_CUDA_KERNEL_LAUNCH_CHECK();
     return std::make_tuple(input_grad, trans_grad);
   }
 
-  dim3 grid(std::min(THCCeilDiv(out_size, 512L), 4096L));
+  dim3 grid(std::min((out_size + 511L) / 512L, 4096L));
   dim3 block(512);
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
@@ -414,6 +411,6 @@ deform_psroi_pooling_cuda_backward(const at::Tensor &out_grad,
         num_classes,
         channels_each_class);
   });
-  THCudaCheck(cudaGetLastError());
+  C10_CUDA_KERNEL_LAUNCH_CHECK();
   return std::make_tuple(input_grad, trans_grad);
 }
